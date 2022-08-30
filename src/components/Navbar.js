c0b1422c462c737025ae0205cc21d3544ca0c309
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-08-30 12:00:10 +0200

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <p>LOGO</p>
+            <p>Manü</p>
+        </nav>
+    )
+}
+
+export default Navbar
